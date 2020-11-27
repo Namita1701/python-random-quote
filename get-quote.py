e8267b0e9b48aad49c84899a04c689da675be887
Namita Mahindru <namitamahindru@NAMITAs-MBP.fios-router.home>
2020-11-27 16:50:50 -0500

    Random quote bot is working

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,14 @@
 def primitive():
-  print("Keep it logically awesome.")
+  import random
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
-
-  #print(quotes)
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
+  
+  last = len(quotes) - 1
+  rnd = random.randint(0,last)
+  
+  print(quotes[rnd])
 
 if __name__== "__main__":
   primitive()
